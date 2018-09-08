07b04d17b25ccfdc8933e75d227fafcee49a57ea
balazs-varga <balazsvarga91@gmail.com>
2018-09-08 13:09:53 +0200

    feat(project): created RecursiveDirectoryPrinter class skeleton

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.io.File;
+
 public class Main {
 
+    static void RecursiveDirectoryPrinter(File[] array, int index, int dirIndent)
+    {
+        
+    }
+
     public static void main(String[] args) {
-	// write your code here
     }
 }
