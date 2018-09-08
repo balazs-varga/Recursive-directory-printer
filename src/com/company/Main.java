a48f7972e8b53bdd964c9406a2a68f5ec781e467
balazs-varga <balazsvarga91@gmail.com>
2018-09-08 13:14:56 +0200

    feat(project): implemented folder list logic

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,19 @@ public class Main {
 
     static void RecursiveDirectoryPrinter(File[] array, int index, int dirIndent)
     {
-        
+        String dirIntend = " ";
+
+        if(index == array.length)
+            return;
+
+        if(array[index].isDirectory())
+        {
+            System.out.println(dirIntend + "[" + array[index].getName() + "]");
+
+            RecursiveDirectoryPrinter(array[index].listFiles(), 0, dirIndent + 1);
+        }
+
+        RecursiveDirectoryPrinter(array, ++index, dirIndent);
     }
 
     public static void main(String[] args) {
